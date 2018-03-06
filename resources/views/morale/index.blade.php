e96de2d85cccd2495eeea62ec8ff9de671263103
Narciso Batingal <batingalnarz11@gmail.com>
2018-03-06 12:30:58 +0800

    update

--- resources/views/morale/index.blade.php
+++ resources/views/morale/index.blade.php
@@ -383,7 +383,7 @@ var myBarChart = new Chart($('#perQuestionOIChart'), {
                     // the label toward the edge, and positive values toward the center.
                     yAdjust: -15,
                     // Whether the label is enabled and should be displayed
-                    enabled: true,
+                    enabled: false,
                     // Text to display in label - default is null
                     content: "Critical"
                 },
